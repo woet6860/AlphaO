69216ab4be372d7f6a406090171f37b2fc6ad0c5
chamsom <>
2021-07-28 17:18:50 +0900

    remove_liberty 와 string 나머지 구현해야함 for 문에 있어서 줄일수 있는 방안이 있는지 확인

--- AlphaO/src/oboard/Board.java
+++ AlphaO/src/oboard/Board.java
@@ -5,13 +5,15 @@ import oboard.Ostring;
 import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
 public class Board {
-	int num_raws;
+	int num_rows;
 	int num_cols;
-    public Map<Point, Player>_grid = new HashMap<>();	
+
+    public Map<Point, Ostring[]>_grid = new HashMap<>();	
 	private Board() {
-		this.num_raws = num_raws;
+		this.num_rows = num_rows;
 		this.num_cols = num_cols;
 		this._grid = _grid;
 		
@@ -21,69 +23,129 @@ public class Board {
 		assert this.is_on_grid(point);
 		assert this._grid.get(point) == null;
 		
-		
-        //HashSet<Point> adjacent_same_color_ULRL = new HashSet<>();
-    	HashSet<Point> adjacent_same_color_URLL = new HashSet<>();
-    
-    	//adjacent_same_color_ULRL.add(point);
-    	HashSet<Point> liberties_URLL = new HashSet<>(); 
+		HashSet<Ostring> adjacent_same_color_URLL = new HashSet<>();
+		HashSet<Ostring> adjacent_same_color_ULRL = new HashSet<>();
+        HashSet<Ostring> adjacent_same_color_HORIZON = new HashSet<>();
+        HashSet<Ostring> adjacent_same_color_VERTICAL = new HashSet<>();
     	
-    	///Ostring ostring_ULRL;// '\'
+    
+    	HashSet<Point> liberties_URLL = new HashSet<>();
+    	HashSet<Point> liberties_ULRL = new HashSet<>(); 
+    	HashSet<Point> liberties_HORIZON = new HashSet<>(); 
+    	HashSet<Point> liberties_VERTICAL = new HashSet<>(); 
+
 		//URLL
 		for(Point neighbor:point.neighbor_URLL) {
-			
-		    
 			if(this.is_on_grid(neighbor) == false) {
 				continue;
 			}
-			Ostring neighbor_string = new Ostring(Player, );
-			
-            neighbor_string.color = this._grid.get(neighbor);
-			
+			Ostring neighbor_string = this._grid.get(neighbor)[1];		
 			if(neighbor_string.color == null) {
 				liberties_URLL.add(neighbor);
 			}
 			else if(neighbor_string.color == player) {
-				if(adjacent_same_color_URLL.contains(neighbor_string) == false) {			
-             adjacent_same_color_URLL.addAll(neighbor_string.stones);}                
+			if(adjacent_same_color_URLL.contains(neighbor_string) == false) {
+             adjacent_same_color_URLL.add(neighbor_string);}                
 			}
+	     	else {
+		    	
+		   }
+		}
+		HashSet<Point> new_stones_URLL = new HashSet<>();
+		new_stones_URLL.add(point);
+        Ostring new_string_URLL = new Ostring(player,new_stones_URLL, liberties_URLL);	
+		for(Ostring same_color_string : adjacent_same_color_URLL) {
+        	new_string_URLL = new_string_URLL.merged_with(same_color_string);
+        }
+		//URLL-END
+		
+		//ULRL
+		for(Point neighbor:point.neighbor_ULRL){
+			if(this.is_on_grid(neighbor) == false) {
+				continue;
+			}
+			Ostring neighbor_string = this._grid.get(neighbor)[2];		
+			if(neighbor_string.color == null) {
+				liberties_ULRL.add(neighbor);
+			}
+			else if(neighbor_string.color == player) {
+			if(adjacent_same_color_ULRL.contains(neighbor_string) == false) {
+             adjacent_same_color_ULRL.add(neighbor_string);}                
+			}
+	     	else {
+		    	
+		   }
+		}
+		//ULRL-END
+		
+		//HORIZON
+		for(Point neighbor:point.neighbor_HORIZON) {
+			if(this.is_on_grid(neighbor) == false) {
+				continue;
+			}
+			Ostring neighbor_string = this._grid.get(neighbor)[3];		
+			if(neighbor_string.color == null) {
+				liberties_HORIZON.add(neighbor);
+			}
+			else if(neighbor_string.color == player) {
+			if(adjacent_same_color_HORIZON.contains(neighbor_string) == false) {
+             adjacent_same_color_HORIZON.add(neighbor_string);}                
+			}
+	     	else {
+		    	
+		   }
+		}
+		//HORIZON-END
+		
+		//VERTICAL
+		for(Point neighbor:point.neighbor_VERTICAL) {
+			if(this.is_on_grid(neighbor) == false) {
+				continue;
+			}
+			Ostring neighbor_string = this._grid.get(neighbor)[4];		
+			if(neighbor_string.color == null) {
+				liberties_VERTICAL.add(neighbor);
+			}
+			else if(neighbor_string.color == player) {
+			if(adjacent_same_color_VERTICAL.contains(neighbor_string) == false) {
+             adjacent_same_color_VERTICAL.add(neighbor_string);}                
+			}
+	     	else {
+		    	
+		   }
 		}
 		
-		HashSet<Point> URLL_Point = new HashSet<>();
-		Ostring ostring_URLL = new Ostring(player,URLL_Point ,liberties_URLL);
-		Ostring same_color_string;
+		//VERTIVAL-END
 		
-		while(adjacent_same_color_URLL.contains(same_color_string.stones)){
-			ostring_URLL = ostring_URLL.merged_with(same_color_string);
-		}
-			
+		
+		
+       
+	    Ostring[] new_value = {new_string_URLL,};///나머지 추가	
+        for(Point new_string_URLL_point:new_string_URLL.stones) {
+             this._grid.put(new_string_URLL_point,new_value);
+        }
 		
 	
 
 		
-		////////////
-		
-		////////
+		//////////
+		//////////
 		
 	
 		
 	}
-	public Player get(Point point) {
-        Player string = this._grid.get(point) ;
+	public Player get_URLL(Point point) {
+        Ostring string = this._grid.get(point)[1] ;
 		if(string == null) {
 			return null;
 		}
-		return string;
+		return string.color;
 		
 	}
 	
-    public Ostring get_ostring(Point point) {
-    	Ostring string = this._grid
-    }
-	
-	
+
 	
    public boolean is_on_grid(Point point) {
-	  return 1 <= point.raw && point.raw<= this.num_raws && 1 <= point.col && point.col<= this.num_cols;
+	  return 1 <= point.row && point.row<= this.num_rows && 1 <= point.col && point.col<= this.num_cols;
    }
 }
--- AlphaO/src/otypes/Point.java
+++ AlphaO/src/otypes/Point.java
@@ -3,21 +3,42 @@ import java.util.HashSet;
 
 
 public class Point {
-    public int raw;
+    public int row;
     public int col;
-    public HashSet<Point>neighbor_URLL = new HashSet<>();;
+    public HashSet<Point>neighbor_URLL = new HashSet<>();
+    public HashSet<Point>neighbor_ULRL = new HashSet<>();
+    public HashSet<Point>neighbor_HORIZON = new HashSet<>();
+    public HashSet<Point>neighbor_VERTICAL = new HashSet<>();
 
-	public Point(int raw, int col){
-		this.raw = raw;
+	public Point(int row, int col){
+		this.row = row;
 		this.col = col;
 
 	} 
 	public void neighbor_URLL_point(){
-      Point neighbor_URLL_point0 = new Point(this.raw+1,this.col+1);
-      Point neighbor_URLL_point1 = new Point(this.raw-1,this.col-1);
+      Point neighbor_URLL_point0 = new Point(this.row+1,this.col+1);
+      Point neighbor_URLL_point1 = new Point(this.row-1,this.col-1);
       neighbor_URLL.add(neighbor_URLL_point0);
       neighbor_URLL.add(neighbor_URLL_point1);
 	}
+	public void neighbor_ULRL_point(){
+      Point neighbor_URLL_point0 = new Point(this.row-1,this.col+1);
+      Point neighbor_URLL_point1 = new Point(this.row+1,this.col-1);
+      neighbor_URLL.add(neighbor_URLL_point0);
+	  neighbor_URLL.add(neighbor_URLL_point1);
+		}
+	public void neighbor_HORIZON_point(){
+      Point neighbor_URLL_point0 = new Point(this.row,this.col+1);
+	  Point neighbor_URLL_point1 = new Point(this.row,this.col-1);
+	  neighbor_URLL.add(neighbor_URLL_point0);
+	  neighbor_URLL.add(neighbor_URLL_point1);
+			}
+	public void neighbor_VERTICAL_point(){
+	  Point neighbor_URLL_point0 = new Point(this.row+1,this.col);
+      Point neighbor_URLL_point1 = new Point(this.row-1,this.col);
+	  neighbor_URLL.add(neighbor_URLL_point0);
+	  neighbor_URLL.add(neighbor_URLL_point1);
+				}
 }
 
 
